deaf96dd5b36570114cdbb16437b7029eb44a300
tooplick <id6543156918@gmail.com>
2025-10-19 14:08:02 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,9 +1,3 @@
 from .plugin import plugin
-from fastapi import APIRouter
 
 __all__ = ["plugin"]
-@plugin.mount_router()
-def create_router() -> APIRouter:
-    """创建并配置插件路由"""
-    from .router import router
-    return router
